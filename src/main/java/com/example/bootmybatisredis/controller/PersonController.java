ca817a4e1e07948237c8b86367eb54fb6a24b4a1
mac <2734756709@qq.com>
2019-09-09 16:08:27 +0800

    使用统一restful返回结果

--- src/main/java/com/example/bootmybatisredis/controller/PersonController.java
+++ src/main/java/com/example/bootmybatisredis/controller/PersonController.java
@@ -1,5 +1,7 @@
 package com.example.bootmybatisredis.controller;
 
+import com.example.bootmybatisredis.Utils.Result;
+import com.example.bootmybatisredis.Utils.ResultCode;
 import com.example.bootmybatisredis.domain.Person;
 import com.example.bootmybatisredis.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,25 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping("/person/{id}")
-    public Person getPersonById(@PathVariable("id") Integer id) {
-        return personService.selectPersonById(id);
-    }
+//    @GetMapping("/person/{id}")
+//    public Person getPersonById(@PathVariable("id") Integer id) {
+//        Person person = personService.selectPersonById(id);
+//        return person;
+//    }
 
+
+    @GetMapping("/person/{id}")
+    public Result getPersonById(@PathVariable("id") Integer id) {
+        Person person = new Person();
+
+        if (id.equals(0)) {
+            return Result.failure(ResultCode.FAILURE);
+        } else if (id.equals(3)) {
+            return Result.failure(ResultCode.FAILURE);
+        } else {
+            person = personService.selectPersonById(id);
+            return Result.success(person);
+        }
+
+    }
 }
